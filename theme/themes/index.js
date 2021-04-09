8a9906af8465359f3c5466941653a414f1287221
kokanek <kapeel.kokane@gmail.com>
2021-04-09 17:21:48 +0530

    fixing state of css bugs

--- theme/themes/index.js
+++ theme/themes/index.js
@@ -1,9 +1,7 @@
 import { testTheme } from './test';
 import { stateOfJSTheme } from './state_of_js/theme/index.ts'
-import { stateOfCSSTheme } from './state_of_css/theme/index.ts'
 
 export default {
   state_of_js: stateOfJSTheme,
-  state_of_css: stateOfCSSTheme,
   test: testTheme,
 }
